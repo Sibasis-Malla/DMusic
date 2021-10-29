11d86d28afe21cf71b08fc03f3ef9dd840346e6c
Sibasis-Malla <120cs0179@nitrkl.ac.in>
2021-10-30 02:01:47 +0530

    Added,Tested,Deployed Contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,22 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var LicenseToken = artifacts.require("./LicenseToken.sol");
+// var MyToken = artifacts.require("./MyToken.sol");
+// var MyTokenSales = artifacts.require("./MyTokenSale.sol");
 
-module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
+const BigNumber = require('bignumber.js');
+require('dotenv').config();
+let x = new BigNumber(1000*10**13);
+
+
+
+module.exports = async function(deployer) {
+let addr = await web3.eth.getAccounts();
+await deployer.deploy(MyToken, 1000);
+
+await deployer.deploy(MyTokenSales, 4, addr[0], MyToken.address);
+let tokenInstance = await MyToken.deployed();
+await tokenInstance.transfer(MyTokenSales.address, x.toFixed());
+await deployer.deploy(LicenseToken);
+
 };
+// module.exports = function(deployer) {
+// };
