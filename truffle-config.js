d9620736955fbf96a0334c3831ab9ba9a3933281
Sibasis-Malla <120cs0179@nitrkl.ac.in>
2021-11-01 10:45:21 +0530

    deplyoed on matic

--- truffle-config.js
+++ truffle-config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 require('dotenv').config();
 const key = process.env.MNEMONIC;
-const HDWalletProvider = require('truffle-hdwallet-provider');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -38,6 +38,16 @@ module.exports = {
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
+    matic:{
+      networkCheckTimeout: 10000,
+      provider: () => new HDWalletProvider(`${key}`,`https://polygon-mumbai.infura.io/v3/1d5fb752e905416ea938459ac8872368`),
+      network_id:  80001,       // Ropsten's id
+      gas: 5500000,        // Ropsten has a lower block limit than mainnet
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+
+    }
   },
   compilers: {
     solc: {
